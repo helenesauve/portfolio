34202f662d41e5aba59e696dcb8f6b3906b0bdbf
Helene <helenesauveparis@gmail.com>
2023-03-05 18:58:04 +0000

    adding links to github, linkedin and cv

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,6 +7,11 @@ function Hero(props) {
     <div className="hero text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
       <h1>Hey, I'm Hélène</h1>
       <h2>Front-End Web Developer based in Bristol.</h2>
+      <div className="button-list">
+      <a class="btn" href="https://github.com/helenesauve" target="_blank" rel="noreferrer">Github</a>
+      <a class="btn" href="https://www.linkedin.com/in/h%C3%A9l%C3%A8ne-sauv%C3%A9-b572a9117/" target="_blank" rel="noreferrer">LinkedIn</a>
+      <a class="btn" href="https://docs.google.com/document/d/181FQ4xKs4-JjvKeJRMC7BktjksHv1J1I/edit" target="_blank" rel="noreferrer">CV</a>
+      </div>
     </div>
     
   );
