ea3da27139ac9b1b7a5b75bc40d264f587e2920f
Helene <helenesauveparis@gmail.com>
2023-03-16 17:51:42 +0000

    replacing navbar with react-bootstrap responsive navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,59 +3,57 @@ import { Link, NavLink } from "react-router-dom";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
 
-function Navbar () {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light">
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+function Navigation() {
+  return (
+    <Navbar className="Navbar" expand="lg">
+      <Container>
         <Link className="navbar-brand" to="/">
-        <FontAwesomeIcon icon={solid('house')} />
+          <FontAwesomeIcon icon={solid('house')} />
         </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
-      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        {/* <div> */}
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <NavLink
-                to="/projects"
-                end
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Projects
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/contact"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                Contact
-                </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/resume"
-                className={({ isActive }) =>
-                  isActive ? 'nav-link active' : 'nav-link'
-                }
-              >
-                CV
-                </NavLink>
-            </li>
-          </ul>
-        </div>
-        {/* </div> */}
-      </nav>
-    );
-  }
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink
+              to="/projects"
+              end
+              className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }
+            >
+              Contact
+            </NavLink>
+            <NavLink
+              to="/resume"
+              className={({ isActive }) =>
+                isActive ? 'nav-link active' : 'nav-link'
+              }
+            >
+              CV
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-export default Navbar;
+export default Navigation;
+
